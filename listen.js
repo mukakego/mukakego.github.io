228181a8261e38c5cd532280c8c07e5171703a9b
Mukakego <32166690+mukakego@users.noreply.github.com>
2018-04-30 17:26:42 +0900

    speech

--- listen.js
+++ listen.js
@@ -1,8 +1,25 @@
 $(function () {
+    let ss = window.speechSynthesis;
+    let msg = new SpeechSynthesisUtterance();
+    msg.lang = "ja-JP";
+    
+    ss.onvoiceschanged = function(){
+        ss.onvoiceschanged = null;
+        let voices = ss.getVoices();
+        $.each(voices,function(index,value){
+            if(value.voiceURI==="Google 日本語"){
+                msg.voice = value;
+            }
+        })
+    }
+
     if (annyang) {
         annyang.setLanguage('ja');
         annyang.addCallback('result', function(userSaid) {
-            $('#result').append('<br>'+userSaid[0]);
+            let text = userSaid[0];
+            $('#result').append('<br>'+text);
+            msg.text = text;
+            ss.speak(msg);
             //userSaidはStringの配列となっている
             //複数の候補が可能性の高い順に格納されている
             //lengthは一定ではなく1の時もある
