39302beeaf50b47a2aaae94f8c34fa0d4db93aa8
Mukakego <32166690+mukakego@users.noreply.github.com>
2018-04-30 16:12:42 +0900

    webSpeechRecognitionのテスト

---
+++ listen.js
@@ -0,0 +1,12 @@
+$(function(){
+    let recognition = new webkitSpeechRecognition();
+    recognition.lang = "ja";
+    $('#start-btn').on('click', function() {
+        recognition.start();
+    });
+    recognition.addEventListener('result', function(e) {
+        let text = e.results[0][0].transcript;
+        $("#result").append("<br>"+text)
+        recognition.start();
+    })
+})
