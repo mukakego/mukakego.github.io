f50a1c424a2c77d8e0a14e50d60785ef29720dff
Mukakego <32166690+mukakego@users.noreply.github.com>
2018-04-30 16:33:43 +0900

    stop

--- listen.js
+++ listen.js
@@ -1,14 +1,20 @@
 $(function(){
     let recognition = new webkitSpeechRecognition();
     recognition.lang = "ja";
+    let recording = false;
     $('#start-btn').on('click', function() {
+        recording = true;
         recognition.start();
+    });
+    $('#stop-btn').on('click', function() {
+        recording = false;
+        recognition.stop();
     });
     recognition.addEventListener('result', function(e) {
         let text = e.results[0][0].transcript;
         $("#result").append("<br>"+text)
     })
     recognition.onaudioend = function(){
-        recognition.start();
+        recording && recognition.start();
     }
 })
