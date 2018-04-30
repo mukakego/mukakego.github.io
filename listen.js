28ab1053b08cfca4bf511d90a8f24b3fb62b2d0f
Mukakego <32166690+mukakego@users.noreply.github.com>
2018-04-30 17:16:13 +0900

    debug

--- listen.js
+++ listen.js
@@ -2,14 +2,19 @@ $(function () {
     if (annyang) {
         annyang.setLanguage('ja');
         annyang.addCallback('result', function(userSaid) {
-            $('#result').append('<br>'+userSaid);
+            $('#result').append('<br>'+userSaid[0]);
+            //userSaidはStringの配列となっている
+            //複数の候補が可能性の高い順に格納されている
+            //lengthは一定ではなく1の時もある
         });
 
         $('#start-btn').on('click', function () {
             annyang.start();
+            $('#sound').text("録音中")
         });
         $('#stop-btn').on('click', function () {
             annyang.pause();
+            $('#sound').text("停止中")
         });
     }
 })
